072f88187e60a301f38a04a0d6a2ac7fe2fedf7d
Marina Mota <marina.mota.merlo@gmail.com>
2020-04-08 20:19:15 +0200

    Created the file to run annotation in Prokka

---
+++ 4-annotation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#SBATCH -A g2020008
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 00:30:00
+#SBATCH -J assembly_evaluation
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user Marina.Motamerlo.2433@student.uu.se
+# Load modules
+module load bioinfo-tools
+module load prokka
+# Your commands
+prokka assembly/DNA_raw_assembly.contigs.fasta --outdir annotation/prokka_annotation --prefix DNA_annotated
