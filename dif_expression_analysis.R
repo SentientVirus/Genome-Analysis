d266d45d862a5a688b3fa37916c1da94a4f989f5
Marina Mota <marina.mota.merlo@gmail.com>
2020-04-15 16:05:41 +0200

    Removed FASTA genome from gff file
    Simplified the code because I added soft links, changed the way to set the number of threads
    
    Modified name of the output folder in 1 and simplified path to files in 2
    
    To run it on the first contig
    
    Removed non-esential parts
    
    Added files for R analysis
    
    Simplified paths
    
    Removed exact copies
    
    Added reference for annotation
    
    Removed old files
    
    Removed old files
    
    Removed slurm file
    
    Added R code for differential expression analysis
    
    Delete .gitignore
    
    File for FastQC read quality control
    
    Completed the code adding files to compare to and the right module
    
    Changed numbers to follow logical order
    
    Deleted old files
    
    Added new file
    
    Delete duplicate file
    
    Removed wrong file
    
    Readded file that was accidentaly deleted
    
    Added PCA plot
    
    Removed unnecessary parts
    
    Several differences in expression
    
    Volcano plot added
    
    Moved images to a specific folder, added a PCA plot
    
    Removed duplicated plots
    
    Added fastqc files
    
    Removed file with the same aim as other

--- dif_expression_analysis.R
+++ dif_expression_analysis.R
@@ -2,15 +2,16 @@ library(DESeq2)
 library(ggplot2)
 directory <- "F:/Master_Uppsala/Year_1/Semester_2/Genome_Analysis/Project"
 setwd(directory)
-countData <- read.csv('data_CDS.csv', header = TRUE, sep = ";")
+countData <- read.csv('data.csv', header = TRUE, sep = ";")
 head(countData)
 metaData <- read.csv('../metadata.csv', header = TRUE, sep = ";")
 metaData
-dds <- DESeqDataSetFromMatrix(countData=countData,
-                              colData=metaData,
+dds <- DESeqDataSetFromMatrix(countData=countData, 
+                              colData=metaData, 
                               design=~Type, tidy = TRUE)
 dds <- DESeq(dds)
 res <- results(dds)
+head(results(dds, tidy=TRUE))
 summary(res)
 res <- res[order(res$padj),]
 head(res)
@@ -23,7 +24,7 @@ plotCounts(dds, gene="CMHJGECF_01283", intgroup="Type") #COX1 domain-containing 
 plotCounts(dds, gene="CMHJGECF_00315", intgroup="Type") #Flagellin
 plotCounts(dds, gene="CMHJGECF_01746", intgroup="Type") #Heavy metal efflux pump
 
-# Reset par
+#reset par
 par(mfrow=c(1,1))
 # Make a basic volcano plot
 with(res, plot(log2FoldChange, -log10(pvalue), pch=20, main="Volcano plot", xlim=c(-3,3)))
@@ -31,7 +32,3 @@ with(res, plot(log2FoldChange, -log10(pvalue), pch=20, main="Volcano plot", xlim
 # Add colored points: blue if padj<0.01, red if log2FC>1 and padj<0.05)
 with(subset(res, padj<.01 ), points(log2FoldChange, -log10(pvalue), pch=20, col="blue"))
 with(subset(res, padj<.01 & abs(log2FoldChange)>2), points(log2FoldChange, -log10(pvalue), pch=20, col="red"))
-
-# PCA plot
-vsdata <- vst(dds, blind=FALSE)
-plotPCA(vsdata, intgroup="Type")
