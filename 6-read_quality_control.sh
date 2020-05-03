268e83d882b1eb773bbd386eca0fe4bea0dda0be
Marina Mota Merlo <marin@rackham3.uppmax.uu.se>
2020-05-03 23:10:12 +0200

    Completed the code adding files to compare to and the right module

--- 6-read_quality_control.sh
+++ 6-read_quality_control.sh
@@ -1,13 +1,12 @@
-
 #!/bin/bash -l
 #SBATCH -A g2020008
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 00:30:00
+#SBATCH -t 05:00:00
 #SBATCH -J read_quality_control
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user Marina.Motamerlo.2433@student.uu.se
 
 module load bioinfo-tools
-module load fastqc
-fastqc fastqc/* -t 2
+module load FastQC
+fastqc 3_Christel_2017/RNA_raw/ERR20* fastqc/* -t 2
