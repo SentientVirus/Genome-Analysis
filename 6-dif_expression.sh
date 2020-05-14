5e25126cb29942bc51ed723b6c40e73ac577a56e
Marina <Marina.Motamerlo.2433@student.uu.se>
2020-05-14 14:35:28 +0200

    Removed unnecessary parts

--- 6-dif_expression.sh
+++ 6-dif_expression.sh
@@ -13,8 +13,8 @@ module load samtools
 module load htseq
 # Your commands
 bwa index assembly/DNA_raw_assembly.contigs.fasta
-bwa mem -t 4 assembly/DNA_raw_assembly.contigs.fasta trimmomatic/ERR2036629_P1.trim.fastq.gz trimmomatic/ERR2036629_P2.trim.fastq.gz | samtools sort | htseq-count -f bam -r pos -s no -t CDS -i ID - prokka/lept.gff > ERR2036629.txt
-bwa mem -t 4 assembly/DNA_raw_assembly.contigs.fasta trimmomatic/ERR2036630_P1.trim.fastq.gz trimmomatic/ERR2036630_P2.trim.fastq.gz | samtools sort | htseq-count -f bam -r pos -s no -t CDS -i ID - prokka/lept.gff > ERR2036630.txt
-bwa mem -t 4 assembly/DNA_raw_assembly.contigs.fasta trimmomatic/ERR2036631_P1.trim.fastq.gz trimmomatic/ERR2036631_P2.trim.fastq.gz | samtools sort | htseq-count -f bam -r pos -s no -t CDS -i ID - prokka/lept.gff > ERR2036631.txt
-bwa mem -t 4 assembly/DNA_raw_assembly.contigs.fasta trimmomatic/ERR2036632_P1.trim.fastq.gz trimmomatic/ERR2036632_P2.trim.fastq.gz | samtools sort | htseq-count -f bam -r pos -s no -t CDS -i ID - prokka/lept.gff > ERR2036632.txt
-bwa mem -t 4 assembly/DNA_raw_assembly.contigs.fasta trimmomatic/ERR2036633_P1.trim.fastq.gz trimmomatic/ERR2036633_P2.trim.fastq.gz | samtools sort | htseq-count -f bam -r pos -s no -t CDS -i ID - prokka/lept.gff > ERR2036633.txt
+bwa mem -t 4 assembly/DNA_raw_assembly.contigs.fasta trimmomatic/ERR2036629_P1.trim.fastq.gz trimmomatic/ERR2036629_P2.trim.fastq.gz | htseq-count -f bam -r pos -s no -t CDS -i ID - prokka/lept.gff > ERR2036629.txt
+bwa mem -t 4 assembly/DNA_raw_assembly.contigs.fasta trimmomatic/ERR2036630_P1.trim.fastq.gz trimmomatic/ERR2036630_P2.trim.fastq.gz | htseq-count -f bam -r pos -s no -t CDS -i ID - prokka/lept.gff > ERR2036630.txt
+bwa mem -t 4 assembly/DNA_raw_assembly.contigs.fasta trimmomatic/ERR2036631_P1.trim.fastq.gz trimmomatic/ERR2036631_P2.trim.fastq.gz | htseq-count -f bam -r pos -s no -t CDS -i ID - prokka/lept.gff > ERR2036631.txt
+bwa mem -t 4 assembly/DNA_raw_assembly.contigs.fasta trimmomatic/ERR2036632_P1.trim.fastq.gz trimmomatic/ERR2036632_P2.trim.fastq.gz | htseq-count -f bam -r pos -s no -t CDS -i ID - prokka/lept.gff > ERR2036632.txt
+bwa mem -t 4 assembly/DNA_raw_assembly.contigs.fasta trimmomatic/ERR2036633_P1.trim.fastq.gz trimmomatic/ERR2036633_P2.trim.fastq.gz | htseq-count -f bam -r pos -s no -t CDS -i ID - prokka/lept.gff > ERR2036633.txt
