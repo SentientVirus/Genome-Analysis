6b35fc2c4cb51cd6b7a638398a272851700099dd
Marina Mota <marina.mota.merlo@gmail.com>
2020-05-24 02:39:09 +0200

    Added more plots and annotated gene category for differential expression

--- dif_expression.R
+++ dif_expression.R
@@ -2,9 +2,10 @@ library(DESeq2)
 library(ggplot2)
 directory <- "F:/Master_Uppsala/Year_1/Semester_2/Genome_Analysis/Project"
 setwd(directory)
-countData <- read.csv('input_files/data_CDS.csv', header = TRUE, sep = ";")
+countData <- read.csv('files/input_files/DeSeq2/data_CDS.csv', header = TRUE, sep = ";")
 head(countData)
-metaData <- read.csv('../metadata.csv', header = TRUE, sep = ";")
+metaData <- read.csv('files/input_files/DeSeq2/metadata.csv', header = TRUE, sep = ";")
+annotations <- read.csv('files/input_files/DeSeq2/lept_annotations.csv', header=TRUE, sep=";")
 metaData
 dds <- DESeqDataSetFromMatrix(countData=countData,
                               colData=metaData,
@@ -14,24 +15,69 @@ res <- results(dds)
 summary(res)
 res <- res[order(res$padj),]
 head(res)
-par(mfrow=c(2,3))
 
-plotCounts(dds, gene="CMHJGECF_02245", intgroup="Type") #Putative adenylylsulfate reductase
-plotCounts(dds, gene="CMHJGECF_01705", intgroup="Type") #DrsE domain-containing protein
-plotCounts(dds, gene="CMHJGECF_01744", intgroup="Type") #Putative outer membrane efflux protein
-plotCounts(dds, gene="CMHJGECF_01283", intgroup="Type") #COX1 domain-containing protein
-plotCounts(dds, gene="CMHJGECF_00315", intgroup="Type") #Flagellin
-plotCounts(dds, gene="CMHJGECF_01746", intgroup="Type") #Heavy metal efflux pump
+data <- plotCounts(dds, gene="CMHJGECF_02245", intgroup="Type", returnData = TRUE) #Putative adenylylsulfate reductase
+data2 <- plotCounts(dds, gene="CMHJGECF_01705", intgroup="Type", returnData = TRUE) #DrsE domain-containing protein
+data3 <- plotCounts(dds, gene="CMHJGECF_01744", intgroup="Type", returnData = TRUE) #Putative outer membrane efflux protein (metal resistance)
+data4 <- plotCounts(dds, gene="CMHJGECF_01283", intgroup="Type", returnData = TRUE) #COX1 domain-containing protein
+data5 <- plotCounts(dds, gene="CMHJGECF_00315", intgroup="Type", returnData = TRUE) #Flagellin (motility)
+data6 <- plotCounts(dds, gene="CMHJGECF_01746", intgroup="Type", returnData = TRUE) #Heavy metal efflux pump (metal resistance)
+
+#Several genes in one plot, nicer format
+data %>%
+  mutate(gene = "Sulfur metabolism") %>%
+  bind_rows(mutate(data2, gene = "Sulfur metabolism")) %>%
+  ggplot(aes(Type, count)) + 
+  geom_point(aes(color = Type, shape = gene)) + 
+  scale_y_log10()
+data3 %>%
+  mutate(gene = "Metal resistance") %>%
+  bind_rows(mutate(data4, gene = "Cytochrome C")) %>%
+  bind_rows(mutate(data5, gene = "Motility")) %>%
+  bind_rows(mutate(data6, gene = "Metal resistance")) %>%
+  ggplot(aes(Type, count)) + 
+  geom_point(aes(color = Type, shape = gene)) + 
+  scale_y_log10()
 
 # Reset par
 par(mfrow=c(1,1))
 # Make a basic volcano plot
-with(res, plot(log2FoldChange, -log10(pvalue), pch=20, main="Volcano plot", xlim=c(-3,3)))
+with(res, plot(log2FoldChange, -log10(padj), pch=20, main=expression("Volcano plot: p"['value']), ylim=c(0,70),
+               xlab=expression(log[2](FoldChange)), ylab=expression(-log[10](p['adj']))))
+mtext("Continuous", side = 1, line=3, at=-4)
+mtext("Mineral", side = 1, line=3, at=4)
 
 # Add colored points: blue if padj<0.01, red if log2FC>1 and padj<0.05)
-with(subset(res, padj<.01 ), points(log2FoldChange, -log10(pvalue), pch=20, col="blue"))
-with(subset(res, padj<.01 & abs(log2FoldChange)>2), points(log2FoldChange, -log10(pvalue), pch=20, col="red"))
+with(subset(res, padj<.01 ), points(log2FoldChange, -log10(padj), pch=20, col="blue"))
+with(subset(res, padj<.01 & abs(log2FoldChange)>2), points(log2FoldChange, -log10(padj), pch=20, col="red"))
+
+par(mfrow=c(1,1))
+#Plot with manual annotations
+with(res, plot(log2FoldChange, -log10(padj), pch=20, main="Volcano plot: gene category", ylim=c(0,70),
+               xlab=expression(log[2](FoldChange)), ylab=expression(-log[10](p['adj']))))
+mtext("Continuous", side=1, line=3, at=-4)
+mtext("Mineral", side=1, line=3, at=4)
+atp <- subset(annotations, Category=="ATP synthase", select=CDS)$CDS
+chem <- subset(annotations, Category=="Chemotaxis", select=CDS)$CDS
+met <- subset(annotations, Category=="Metal resistance", select=CDS)$CDS
+mot <- subset(annotations, Category=="Motility", select=CDS)$CDS
+nad <- subset(annotations, Category=="NADH dehydrogenase", select=CDS)$CDS
+nif <- subset(annotations, Category=="Nitrogen fixation", select=CDS)$CDS
+pol <- subset(annotations, Category=="Polysaccharide", select=CDS)$CDS
+tca <- subset(annotations, Category=="TCA cycle", select=CDS)$CDS
+with(res[atp,], points(log2FoldChange, -log10(padj), pch = 3, cex = 1.0, col="red"))
+with(res[chem,], points(log2FoldChange, -log10(padj), pch = 3, cex = 1.0, col="orange"))
+with(res[met,], points(log2FoldChange, -log10(padj), pch = 3, cex = 1.0, col="yellow"))
+with(res[mot,], points(log2FoldChange, -log10(padj), pch = 3, cex = 1.0, col="green"))
+with(res[nad,], points(log2FoldChange, -log10(padj), pch = 3,cex = 1.0, col="cyan"))
+with(res[nif,], points(log2FoldChange, -log10(padj), pch = 3, cex = 1.0, col="blue"))
+with(res[pol,], points(log2FoldChange, -log10(padj), pch = 3, cex = 1.0, col="purple"))
+with(res[tca,], points(log2FoldChange, -log10(padj), pch = 3, cex = 1.0, col="violet"))
+legend("center", pch= 3, cex=0.8, legend=c("ATP synthase", "Chemotaxis", "Metal resistance", "Motility", "NADH DH", "Nitrogen fixation", "Polysaccharide", "TCA cycle"),
+       col=c("red", "orange", "yellow", "green", "cyan", "blue", "purple", "violet"))
+
 
 # PCA plot
 vsdata <- vst(dds, blind=FALSE)
 plotPCA(vsdata, intgroup="Type")
+
